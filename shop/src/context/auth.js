f020901d97bdf58f6d172b075deab520f523d57e
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-28 07:25:15 +0600

    Login working

--- shop/src/context/auth.js
+++ shop/src/context/auth.js
@@ -2,14 +2,14 @@ import { useState, useContext, createContext } from 'react'
 
 const AuthContext = createContext();
 
-const AuthProvider = ({ Children }) => {
+const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({
         user: null,
         token: ''
     })
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {Children}
+        <AuthContext.Provider value={[auth, setAuth]}>
+            {children}
         </AuthContext.Provider>
     )
 }
@@ -18,4 +18,44 @@ const AuthProvider = ({ Children }) => {
 // custon hook
 const useAuth = () => useContext(AuthProvider)
 
-export { AuthProvider, useAuth }
+export { useAuth, AuthProvider };
+
+
+
+
+// import { useState, useEffect, useContext, createContext } from "react";
+// import axios from "axios";
+
+// const AuthContext = createContext();
+// const AuthProvider = ({ children }) => {
+//     const [auth, setAuth] = useState({
+//         user: null,
+//         token: "",
+//     });
+
+//     //default axios
+//     axios.defaults.headers.common["Authorization"] = auth?.token;
+
+//     useEffect(() => {
+//         const data = localStorage.getItem("auth");
+//         if (data) {
+//             const parseData = JSON.parse(data);
+//             setAuth({
+//                 ...auth,
+//                 user: parseData.user,
+//                 token: parseData.token,
+//             });
+//         }
+//         //eslint-disable-next-line
+//     }, []);
+//     return (
+//         <AuthContext.Provider value={[auth, setAuth]}>
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// };
+
+// // custom hook
+// const useAuth = () => useContext(AuthContext);
+
+// export { useAuth, AuthProvider };
--- shop/src/pages/Home.js
+++ shop/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout.js'
-
+// import { useAuth } from '../context/auth.js'
 const Home = () => {
+    // const [auth] = useAuth;
     return (
         <Layout title={"Home - "}>
+            {/* {JSON.stringify(auth, null, 4)} */}
             <h1>Home Pages</h1>
         </Layout>
     )
