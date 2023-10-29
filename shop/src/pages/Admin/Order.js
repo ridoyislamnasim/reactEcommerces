f97d984b3162cb905ea0dd9da276fc782df11871
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-30 04:48:02 +0600

    user admin Order  page

---
+++ shop/src/pages/Admin/Order.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Order = () => {
+    return (
+        <div>
+            <Layout title={"Order - "} >
+                <h1>Order Pages</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default Order
