903a8c162d11c8de033508a918561754bfa145c5
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-30 04:25:04 +0600

    user admin information separation

--- shop/src/pages/Admin/AdminDashboard.js
+++ shop/src/pages/Admin/AdminDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import AdminMenu from '../../components/Menu/AdminMenu'
 
 
 const AdminDashboard = () => {
     return (
         <Layout title={"User Dashboard - "} >
             <h1>Admin Dashboard page</h1>
+            <AdminMenu />
         </Layout>
     )
 }
--- shop/src/pages/User/UserDashboard.js
+++ shop/src/pages/User/UserDashboard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import UserMenu from '../../components/Menu/UserMenu'
 
 
 const UserDashboard = () => {
     return (
         <Layout title={"User Dashboard - "} >
             <h1>User Dashboard page</h1>
+            <UserMenu />
         </Layout>
     )
 }
