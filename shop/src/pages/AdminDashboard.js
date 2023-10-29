744d19f9673b08c64d7f745d442bb2e8984fe5d4
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-29 23:05:57 +0600

    admin  dash bord create

---
+++ shop/src/pages/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+
+const AdminDashboard = () => {
+    return (
+        <Layout title={"User Dashboard - "} >
+            <h1>Admin Dashboard page</h1>
+        </Layout>
+    )
+}
+
+export default AdminDashboard
