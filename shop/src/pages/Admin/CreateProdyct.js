f60174e8726ac68a0a675750f09f189c197faf8f
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-30 04:42:11 +0600

    user admin CreateProoduct  page

---
+++ shop/src/pages/Admin/CreateProdyct.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const CreateProdyct = () => {
+    return (
+        <div>
+            <Layout title={"About us - "} >
+                <h1>Create Category</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default CreateProdyct
