b4313fdd1c49ffcd0a40cf00830330fe01017d1f
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-30 04:40:09 +0600

    user admin CreateCategory page

---
+++ shop/src/pages/Admin/CreateCategory.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const CreateCategory = () => {
+    return (
+        <div>
+            <Layout title={"About us - "} >
+                <h1>Create Category</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default CreateCategory
