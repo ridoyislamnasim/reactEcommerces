8562cab4597952b55b1bbee6b75125863ab2e1a5
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-10-29 23:04:48 +0600

    user dash bord create

---
+++ shop/src/pages/UserDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+
+const UserDashboard = () => {
+    return (
+        <Layout title={"User Dashboard - "} >
+            <h1>User Dashboard page</h1>
+        </Layout>
+    )
+}
+
+export default UserDashboard
