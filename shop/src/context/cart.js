87344db25f2d72fb8c39b1e4227be272b4dfbfef
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-11-08 20:43:35 +0600

    react

---
+++ shop/src/context/cart.js
@@ -0,0 +1,73 @@
+import { useState, useContext, createContext } from 'react'
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+
+    // const [cart, setCart] = useState()
+    const [cart, setCart] = useState([]);
+    // useEffect(() => {
+    //     const data = localStorage.getItem("cart");
+    //     if (data) {
+    //         const parseData = JSON.parse(data);
+    //         setCart({
+    //             ...cart,
+    //             user: parseData.user,
+    //             token: parseData.token,
+    //         });
+    //     }
+    //     // eslint-disable-next-line
+    // }, []);
+
+    return (
+        <CartContext.Provider value={[cart, setCart]}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+
+// custon hook
+const useAuthr = () => useContext(CartContext)
+
+export { useAuthr, CartProvider };
+
+
+
+
+// import { useState, useEffect, useContext, createContext } from "react";
+// import axios from "axios";
+
+// const CartContext  = createContext();
+// const CartProvider = ({ children }) => {
+//     const [cart, setCart] = useState({
+//         user: null,
+//         token: "",
+//     });
+
+//     //default axios
+//     axios.defaults.headers.common["Authorization"] = cart?.token;
+
+//     useEffect(() => {
+//         const data = localStorage.getItem("cart");
+//         if (data) {
+//             const parseData = JSON.parse(data);
+//             setCart({
+//                 ...cart,
+//                 user: parseData.user,
+//                 token: parseData.token,
+//             });
+//         }
+//         //eslint-disable-next-line
+//     }, []);
+//     return (
+//         <CartContext .Provider value={[cart, setCart]}>
+//             {children}
+//         </CartContext .Provider>
+//     );
+// };
+
+// // custom hook
+// const useAuth = () => useContext(CartContext );
+
+// export { useAuth, CartProvider };
