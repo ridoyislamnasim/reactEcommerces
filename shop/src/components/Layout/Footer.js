8946ca6a85cf39caf56edab1c19b3cc229758b9f
ridoyislamnasim <ridoyislamnasim@gmail.com>
2023-11-13 14:29:59 +0600

    torat msg eddit

--- shop/src/components/Layout/Footer.js
+++ shop/src/components/Layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <h6 className='text-center d-flex justify-content-center align-items-center ' >All Right Reserved &copy;<div><NavLink to='https://ridoyislamnasim.up.railway.app/'><span style={{ color: '#ff0000' }}>ridoyislamnasim</span></NavLink></div>  </h6>
             <div className='text-center d-flex align-items-center mt-3'>
                 <div><NavLink to='/'>Home</NavLink></div>|
-                <div><NavLink to='/contact'>Contact</NavLink></div>  |
+                <div><NavLink to='/contact'>Contact</NavLink></div>
 
             </div>
         </div>
